e6c2344c906f16e543135b0374975d5c18026c9a
Mayuri <mayu@javacoding.com>
2022-08-15 21:58:21 +0530

    SB-Git - Spring boot Git First commit

---
+++ src/main/java/com/example/demo/sample/TestForGit.java
@@ -0,0 +1,15 @@
+package com.example.demo.sample;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestForGit implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("hello Spring boot !!!!!");
+
+	}
+
+}
