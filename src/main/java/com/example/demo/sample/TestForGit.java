73bcdfc7163b07bf74dd90772a2814c2f4a2e642
Mayuri2394 <111291371+Mayuri2394@users.noreply.github.com>
2022-11-10 16:32:52 +0530

    Update TestForGit.java

--- src/main/java/com/example/demo/sample/TestForGit.java
+++ src/main/java/com/example/demo/sample/TestForGit.java
@@ -9,7 +9,7 @@ public class TestForGit implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("hello Spring boot !!!!!");
-		System.out.println("Welcome to git hub  !!!!!");
+		System.out.println("Welcome to git hub123  !!!!!");
                 
 	}
 
